5e8ba53c87f65f0041a128e262725b3d05ee56b4
NaliotovEvgeniy <151935908+NaliotovEvgeniy@users.noreply.github.com>
2024-08-17 11:14:02 +0300

    ы

---
+++ 12345.js
@@ -0,0 +1,23 @@
+if{
+    console.log("update your version");
+    queueMicrotask(isFinite "loadiang procesion");
+    found old version;
+} else {
+
+}
+if (!params.q) {
+    Notifilix.Notify.failure("Enter question")
+    const API = 
+}
+function getNews({ q, page = 1, pageSize = 5 }) {
+    return axios.get(ENDPOINT, {
+        apiKey: API_Key,
+        q,
+        page,
+        pageSize,
+        language: "en", 
+    })
+    
+}
+export {getNews}
+
